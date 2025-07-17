19c5b06148d4a55a68c5db4e175db448f8289c1d
Timo Behrend <behrendtimo@yahoo.de>
2025-07-17 16:52:13 +0200

    Add memory section symbol definitions and display in kernel
    Introduced macros and declarations in `symbols.h` to define memory section boundaries and sizes. Updated `kernel_main` to print the memory layout using these symbols. Adjusted linker script to explicitly define start and end points for each memory section.

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,6 @@
 #include "kassert.h"
 #include "kstdio.h"
+#include "symbols.h"
 #include "vga.h"
 
 void kernel_main(void) {
@@ -9,6 +10,16 @@ void kernel_main(void) {
 	kprintf("This is VGA text mode, here is some %x to enjoy!\n", 0xDEADBEEF);
 	kprintf("Using %%c you can print out single characters%c\n", '!');
 	kprintf("All invalid formats just get printed out %42!\n");
+
+	kprintf("\n");
+
+	kprintf("Memory Section Map\n");
+	kprintf(" %s: %x - %x (%d bytes)\n", "text", SYMBOL_START(text), SYMBOL_END(text), SYMBOL_SIZE(text));
+	kprintf(" %s: %x - %x (%d bytes)\n", "rodata", SYMBOL_START(rodata), SYMBOL_END(rodata), SYMBOL_SIZE(rodata));
+	kprintf(" %s: %x - %x (%d bytes)\n", "data", SYMBOL_START(data), SYMBOL_END(data), SYMBOL_SIZE(data));
+	kprintf(" %s: %x - %x (%d bytes)\n", "bss", SYMBOL_START(bss), SYMBOL_END(bss), SYMBOL_SIZE(bss));
+
+	kprintf("\n");
 
 	ASSERT_M(42 != 42, "The cake is a lie");
 }
