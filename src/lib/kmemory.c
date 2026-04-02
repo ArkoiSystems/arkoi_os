c10b59424a32752691331cf9b09a36d02f02942d
Timo Behrend <behrendtimo@yahoo.de>
2026-04-02 18:45:26 +0000

    Change memcpy to use uint8_t for better type consistency

--- src/lib/kmemory.c
+++ src/lib/kmemory.c
@@ -1,8 +1,9 @@
 #include "lib/kmemory.h"
+#include <stdint.h>
 
 void *memcpy(void *destination, const void *source, uint32_t size) {
-    char *dest = (char *) destination;
-    const char *src = (char *) source;
+    uint8_t *dest = (uint8_t *) destination;
+    const uint8_t *src = (uint8_t *) source;
 
     while (size--) {
         *dest = *src;
